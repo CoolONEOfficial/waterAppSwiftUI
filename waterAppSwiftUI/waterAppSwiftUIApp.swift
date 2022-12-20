19a77f101ecdd410c491d762b6dd3f0e9216d40e
Nikolai Trukhin <n.trukhin@uzum.com>
2022-12-20 16:45:31 +0100

    Initial Commit

---
+++ waterAppSwiftUI/waterAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterAppSwiftUIApp.swift
+//  waterAppSwiftUI
+//
+//  Created by Nikolay Truhin on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct waterAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
